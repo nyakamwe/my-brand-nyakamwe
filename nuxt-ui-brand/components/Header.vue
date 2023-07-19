88ec5060c1e0104adb095ce87cc01664f31d3293
nyakamwe <nyakamweaimable@gmail.com>
2023-07-19 16:23:09 +0200

    reduce top margin on navigation links

--- nuxt-ui-brand/components/Header.vue
+++ nuxt-ui-brand/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
     <header class="shadow-sm bg-white">
-        <nav class=" container mx-auto p-2 flex justify-between">
+        <nav class=" container mx-auto p-2 flex justify-between items-center">
             <NuxtLink to="/" class="mt-3">NYAKAMWE.</NuxtLink>
             <div class="flex items-center">
-                <NuxtLink to="/" exact-active-class="exact-active" class="mt-3 mx-2">Home</NuxtLink>
-                <NuxtLink to="/work" exact-active-class="exact-active" class="mt-3 mx-2">Work</NuxtLink>
-                <NuxtLink to="/services" exact-active-class="exact-active" class="mt-3 mx-2">Services</NuxtLink>
+                <NuxtLink to="/" exact-active-class="exact-active" class="mt-2 mx-2">Home</NuxtLink>
+                <NuxtLink to="/work" exact-active-class="exact-active" class="mt-2 mx-2">Work</NuxtLink>
+                <NuxtLink to="/services" exact-active-class="exact-active" class="mt-2 mx-2">Services</NuxtLink>
             </div>
             <img class="w-12 h-12 rounded-full" src="https://avatars.githubusercontent.com/u/53573955?s=400&u=15719f1c24e8edb176d3dba0f98c0c907bd541d1&v=4" alt="profile-image">
         </nav>
---
+++ nuxt-ui-brand/pages/_error.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The requested page could not be found.</p>
+    </div>
+  </template>
