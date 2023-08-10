2a82e0289eae2e33964589bf68c13b23ef955f3f
nyakamwe <nyakamweaimable@gmail.com>
2023-08-10 06:19:56 +0200

    Updates

---
+++ _nuxt/config.e28f1d8e.js
@@ -0,0 +1,1 @@
+import{s as o,v as p,h as i,x as s}from"./entry.00139b81.js";const t=o({nuxtIcon:{size:"28px",class:"icon",aliases:{nuxt:"logos:nuxt-icon"}}}),a={},f=p(t,a);function c(){const n=i();return n._appConfig||(n._appConfig=s(f)),n._appConfig}export{c as u};
